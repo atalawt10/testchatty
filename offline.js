0f2178915189776ec2f5f59d002cd8efb232ff9a
atalawt10 <78620973+atalawt10@users.noreply.github.com>
2021-02-05 17:56:47 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1612558237,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bg-sheet0.png",
+		"images/c1b814bed25243a73ec569f532a89b9d-sheet0.png",
+		"images/chorandoderir-sheet0.png",
+		"images/happy-sheet0.png",
+		"images/sorrindo-sheet0.png",
+		"images/piscando-sheet0.png",
+		"images/chorandoooo-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
